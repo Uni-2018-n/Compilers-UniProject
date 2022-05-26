47fdf43d6e869120f6777a82c92fc7139e2397c8
smackflad <ask.kalamakhs@hotmail.com>
2022-05-26 14:39:21 +0300

    started playing with method declaration and fixed utf bug of given code

--- miniJava/thirdVisitor.java
+++ miniJava/thirdVisitor.java
@@ -93,8 +93,8 @@ public class thirdVisitor extends GJDepthFirst<String, String> {
     "declare i8* @calloc(i32, i32) \n" +
     "declare i32 @printf(i8*, ...)\n"+
     "declare void @exit(i32)\n"+
-    "@_cint = constant [4 x i8] c"+"%d\0a\00"+"\n"+
-    "@_cOOB = constant [15 x i8] c"+"Out of bounds\0a\00"+"\n"+
+    "@_cint = constant [4 x i8] c\"%d\\0a\\00\"\n"+
+    "@_cOOB = constant [15 x i8] c\"Out of bounds\\0a\\00\"\n"+
     "define void @print_int(i32 %i) {\n"+
     "%_str = bitcast [4 x i8]* @_cint to i8*\n"+
     "call i32 (i8*, ...) @printf(i8* %_str, i32 %i)\n"+
@@ -139,21 +139,181 @@ public class thirdVisitor extends GJDepthFirst<String, String> {
         String _ret=null;
         String cName = n.f1.accept(this, argu);
         String str = 
-        "@."+cName+"_vtable = global [0 x i8*] [] \n"+
         "define i32 @main() {\n"+
         "ret i32 0\n"+
         "}\n";
+        out.write(str);
         n.f11.accept(this, argu);
         n.f14.accept(this, argu);
         n.f15.accept(this, argu);
         return _ret;
-     }
+    }
 
+    /**
+        * f0 -> "class"
+        * f1 -> Identifier()
+        * f2 -> "{"
+        * f3 -> ( VarDeclaration() )*
+        * f4 -> ( MethodDeclaration() )*
+        * f5 -> "}"
+    */
+    public String visit(ClassDeclaration n, String argu) throws Exception {
+        String _ret=null;
+        String id = n.f1.accept(this, argu);
+        n.f3.accept(this, argu);
+        n.f4.accept(this, id);
+        return _ret;
+    }
+
+    /**
+    * f0 -> "public"
+    * f1 -> Type()
+    * f2 -> Identifier()
+    * f3 -> "("
+    * f4 -> ( FormalParameterList() )?
+    * f5 -> ")"
+    * f6 -> "{"
+    * f7 -> ( VarDeclaration() )*
+    * f8 -> ( Statement() )*
+    * f9 -> "return"
+    * f10 -> Expression()
+    * f11 -> ";"
+    * f12 -> "}"
+    */
+    public String visit(MethodDeclaration n, String argu) throws Exception {
+        String _ret=null;
+        String type = n.f1.accept(this, argu);
+        String id = n.f2.accept(this, argu);
+        String params = n.f4.accept(this, argu);
+        n.f7.accept(this, argu);
+        n.f8.accept(this, argu);
+        n.f10.accept(this, argu);
+
+        String fin = 
+        "define "+ type + " @"+argu+"."+id+"(i8* %this";
+        if(params != null){
+            if(params.indexOf("//") != -1){
+                params = params.substring(0, params.indexOf("//"))+") {\n"+params.substring(params.indexOf("//")+2);
+            }
+            fin += params;
+        }else{
+            fin += ") {\n";
+        }
+        fin += "}\n";
+        out.write(fin);
+        return _ret;
+    }
+
+    /**
+        * f0 -> FormalParameter()
+        * f1 -> FormalParameterTail()
+    */
+    public String visit(FormalParameterList n, String argu) throws Exception {
+        String _ret=null;
+        String temp = n.f0.accept(this, argu);
+        String temp2 = n.f1.accept(this, temp);
+        if(temp2 != null){
+            return temp2;
+        }
+        return temp;
+    }
+
+    /**
+        * f0 -> Type()
+        * f1 -> Identifier()
+    */
+    public String visit(FormalParameter n, String argu) throws Exception {
+        String type = n.f0.accept(this, argu);
+        String id = n.f1.accept(this, argu);
+        if(argu.contains("//")){
+            String temp = argu;
+            int index = temp.indexOf("//");
+            temp = temp.substring(0, index)+", "+type+" %."+id+temp.substring(index);
+            temp += "%"+id+" = alloca "+type+"\n"+
+            "store "+type+" %."+id+", "+type+"* %"+id+"\n";
+            return temp;
+        }else{
+            String temp = ", "+type+" %."+id+
+            "//"+
+            "%"+id+" = alloca "+type+"\n"+
+            "store "+type+" %."+id+", "+type+"* %"+id+"\n";
+            return temp;
+        }
+    }
+
+    /**
+     * f0 -> ( FormalParameterTerm() )*
+    */
+    public String visit(FormalParameterTail n, String argu) throws Exception {
+        return n.f0.accept(this, argu);
+    }
+
+    /**
+     * f0 -> ","
+    * f1 -> FormalParameter()
+    */
+    public String visit(FormalParameterTerm n, String argu) throws Exception {
+        return n.f1.accept(this, argu);
+    }
+
+
+    /**
+    * f0 -> ArrayType()
+    *       | BooleanType()
+    *       | IntegerType()
+    *       | Identifier()
+    */
+    public String visit(Type n, String argu) throws Exception {
+        return n.f0.accept(this, "noid");
+    }
+
+   /**
+    * f0 -> BooleanArrayType()
+    *       | IntegerArrayType()
+    */
+    public String visit(ArrayType n, String argu) throws Exception {
+        return n.f0.accept(this, argu);
+     }
+  
+     /**
+      * f0 -> "boolean"
+      * f1 -> "["
+      * f2 -> "]"
+      */
+     public String visit(BooleanArrayType n, String argu) throws Exception {
+        return "i1*";
+     }
+  
+     /**
+      * f0 -> "int"
+      * f1 -> "["
+      * f2 -> "]"
+    */
+    public String visit(IntegerArrayType n, String argu) throws Exception {
+        return "i32*";
+    }
+
+    /**
+     * f0 -> "boolean"
+    */
+    public String visit(BooleanType n, String argu) throws Exception {
+        return "i1";
+    }
+
+    /**
+     * f0 -> "int"
+    */
+    public String visit(IntegerType n, String argu) throws Exception {
+        return "i32";
+    }
 
     /**
     * f0 -> <IDENTIFIER>
     */
-   public String visit(Identifier n, String argu) throws Exception {
-    return n.f0.tokenImage;
- }
+    public String visit(Identifier n, String argu) throws Exception {
+        if(argu != null && argu.equals("noid")){
+            return "i8*";
+        }
+        return n.f0.tokenImage;
+    }
 }
